193e9bcc9b18456017470293e7c67f730f74fdcb
Rebecca Smith <70491756+rsmith1220@users.noreply.github.com>
2025-03-31 14:27:20 -0600

    verify file view

--- frontend/src/pages/VerifyFile.jsx
+++ frontend/src/pages/VerifyFile.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useState } from "react"
+
+import FileOverlay from "../components/FileOverlay/FileOverlay"
+import { Button } from "@/components/ui/button"
 
 const VerifyFile = () => {
+  const [files, setFiles] = useState([])
   return (
-    <div>VerifyFile</div>
+    <div className="flex w-full justify-center">
+      <FileOverlay
+        props={{
+          name: "Public key",
+          value: files,
+          onChange: (files) => {
+            setFiles(files)
+            console.log(files)
+          },
+        }}>
+        <div className="flex flex-row gap-2 mt-6 justify-end">
+          {/* <Button>Firm</Button> */}
+
+          <Button>Verify</Button>
+        </div>
+      </FileOverlay>
+    </div>
   )
 }
 
