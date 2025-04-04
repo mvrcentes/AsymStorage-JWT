ef9458b247c4cdffe99a929cce760f235c081cf8
Rebecca Smith <70491756+rsmith1220@users.noreply.github.com>
2025-04-03 21:28:12 -0600

    logout button

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,5 @@
-import React, { Children, useEffect, useState } from "react"
+import React, { useEffect, useState } from "react"
+import { useNavigate } from "react-router-dom"
 
 import KeyManager from "../components/KeyManager/KeyManager"
 import FileManage from "./FileManage"
@@ -7,6 +8,7 @@ import { jwtDecode } from "jwt-decode"
 const Home = () => {
   const [userName, setUserName] = useState("")
   const [privateKey, setPrivateKey] = useState("test1")
+  const navigate = useNavigate() // 🔥 aquí estaba el problema
 
   useEffect(() => {
     const token = localStorage.getItem("token")
@@ -16,12 +18,23 @@ const Home = () => {
     }
   }, [])
 
+  const handleLogout = () => {
+    localStorage.removeItem("token")
+    navigate("/auth")
+  }
+
   return (
-    <div className="flex flex-row justify-center items-center gap-4 min-w-screen ">
-      <KeyManager onPrivateKeyGenerated={setPrivateKey}/>
+    <div className="flex flex-row justify-center items-center gap-4 min-w-screen">
+      <KeyManager onPrivateKeyGenerated={setPrivateKey} />
       <div className="flex flex-col gap-8">
         <h1>Welcome {userName}</h1>
-        <FileManage  privateKey={privateKey} onKeyChange={setPrivateKey}/>
+        <FileManage privateKey={privateKey} onKeyChange={setPrivateKey} />
+        <button
+          onClick={handleLogout}
+          className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 transition"
+        >
+          Logout
+        </button>
       </div>
     </div>
   )
